f0ca0eedefdc5156d277e9d742edae8dff05ceb0
AbinayaR <abinayasahana2008@gmail.com>
2026-09-11 10:27:37 +0530

    Implement merging of two integer arrays

---
+++ practise3.java
@@ -0,0 +1,18 @@
+package project;
+public class MergeTwoArrays {
+	public static void main(String[] args) {
+		int[] arr1= {1,2,3,4};
+		int[] arr2 = {5,6,7,8};
+		int[] arr3 = new int[arr1.length+arr2.length];
+		int i;
+		for( i = 0 ; i < arr1.length ; i++) {
+			arr3[i]=arr1[i] ;
+		}
+		for(int j = 0 ; j < arr2.length ; j++,i++) {
+			arr3[i]= arr2[j];
+		}
+		for(int k = 0; k < arr3.length; k++) {
+			System.out.print(arr3[k]+" ");
+		}
+	}
+}
